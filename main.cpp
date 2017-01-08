341fb5aa7c3f90b4be8f8a3468428700cdbd3872
Jose Luis Montaña <ing.jmontana@gmail.com>
2017-01-08 13:23:02 -0500

    Configuring input and output: Reading file from stdin Writing file to stdout.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 
 int main(){
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for(int arr_i = 0;arr_i < n;arr_i++){
-       cin >> arr[arr_i];
+    freopen("output.txt", "a+", stdout);
+    
+    int testCases;
+    scanf("%d", &testCases);
+
+    while(testCases--) {
+        printf("%d\n", testCases);
     }
-    return 0;
 }
