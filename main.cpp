ce70b84f0a37780a7d9cff4b80bc6e3ce05f7718
Jose Luis Montaña <ing.jmontana@gmail.com>
2017-01-08 23:24:31 -0500

    Funciona... pero va muy lento: Necesita mejorar algoritmo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
-#include <cmath>
+//#include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
+// Set matrix to max size.
+long matrix[101][101][101];
+
+long long update(long long x, long long y, long long z, long long val){
+    matrix[x][y][z] = val;
+    return val;
+}
+
+long long query(long long x1, long long y1, long long z1, long long x2, long long y2, long long z2){
+    long long sum = 0;
+    long long x, y, z;
+
+    z = z1;
+    while(z<=z2){
+        y = y1;
+        while(y<=y2){
+            x = x1;
+            while(x<=x2){
+                sum += matrix[x][y][z];
+                x+=1;
+            }
+            y+=1;
+        }
+        z+=1;
+    }
+
+    return sum;
+}
 
 void processCase(int T)
 {
@@ -14,28 +43,30 @@ void processCase(int T)
     char cmd[8];
     scanf("%lld %lld", &n, &m);
 
-    int x1, y1, z1, x2, y2, z2, val;
+    long long x1, y1, z1, x2, y2, z2, val;
 
-    printf("Test case: %d\n", T);
-    printf("N: %lld, M: %lld\n", n, m);
+    //printf("Test case: %d\n", T);
+    //printf("N: %lld, M: %lld\n", n, m);
+
+    memset(matrix, 0, sizeof(matrix));
 
     while (m){
         scanf("%s", cmd);
         if(!strcmp(cmd, "UPDATE")){
             // Execute upate command.
-            scanf(" %d %d %d %d", &x1, &y1, &z1, &val);
-            printf("%s %d %d %d %d\n", cmd, x1, y1, z1, val);
+            scanf(" %lld %lld %lld %lld", &x1, &y1, &z1, &val);
+            update(x1, y1, z1, val);
+            //printf("%s %d %d %d %d\n", cmd, x1, y1, z1, val);
         }
         if(!strcmp(cmd, "QUERY")){
             // Execute query calculation.
-            scanf("%d %d %d %d %d %d", &x1, &y1, &z1, &x2, &y2, &z2);
-            printf("%s %d %d %d %d %d %d\n", cmd, x1, y1, z1, x2, y2, z2);
+            scanf("%lld %lld %lld %lld %lld %lld", &x1, &y1, &z1, &x2, &y2, &z2);
+            long long result = query(x1, y1, z1, x2, y2, z2);
+            printf("%lld\n", result);
+            //printf("%s %d %d %d %d %d %d\n", cmd, x1, y1, z1, x2, y2, z2);
         }
         m = m-1;
     }
-
-    long matrix[n][n][n];
-    memset(matrix, 0, sizeof(matrix));
 }
 
 int main()
